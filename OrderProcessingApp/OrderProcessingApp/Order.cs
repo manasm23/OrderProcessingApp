1db1a6082c96b048948a0c394781848d2fb0e16f
Manas Maratkar -X (mmaratka - CALSOFT PVT LTD at Cisco) <mmaratka@cisco.com>
2020-08-28 20:19:42 +0530

    Initial Commit
    Created the basic structure of the code

---
+++ OrderProcessingApp/OrderProcessingApp/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingApp
+{
+    public class Order
+    {
+        public string OrderID { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public List<Product> Cart { get; set; }
+
+
+        public void AddProduct (Product prod)
+        {
+
+        }
+
+        public string CreateOrder()
+        {
+            return null;
+        }
+    }
+}
---
+++ OrderProcessingApp/OrderProcessingApp/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingApp
+{
+    public abstract class Product
+    {
+        public string ProductID { get; set; }
+        public decimal Price { get; set; }
+    }
+}
